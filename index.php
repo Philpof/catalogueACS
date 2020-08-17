c99b44d5b722f957ae06db23b35de6f8d086b9f9
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-08-17 21:50:14 +0200

    Login et Logout

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <?php
-session_start ();
+// On prolonge la session
+session_start();
+
 include "header.php";
+
+// On teste si la variable de session existe et contient une valeur
+if(!isset($_SESSION['login']))
+{
+  // Si inexistante ou nulle, on redirige vers le formulaire de login
+  $client = " à vous, visiteur(euse)";
+  $connexion = "Se connecter à mon compte";
+  $goLog = "login";
+}
+else {
+  $client = ", " . $_SESSION['prenom'] . " " . $_SESSION['nom'];
+  $connexion = "Me déconnecter";
+  $goLog = "logout";
+}
 ?>
 
-
-
-
-
-
+<!-- Le HTML -->
 <nav>
   <div class="container-fluid">
     <div class="row justify-content-around">
       <a id="logo" ref="index.php">Logo</a>
-      <a id="login" href="login.php">Se connecter</a>
+      <p id="client">Bonjour<?php echo $client ?> !</p>
+      <a id="connexion" href="<?php echo $goLog ?>.php"><?php echo $connexion ?></a>
       <a id="panier" href="panier.php">Panier</a>
     </div>
   </div>
--- login.php
+++ login.php
@@ -1,78 +1,102 @@
 <?php
-  include "header.php";
+include "header.php";
 
-  // Requête PHP pour vérifier les informations
-  $sql = "SELECT * FROM users WHERE id = '1'";
-  foreach  ($bdd->query($sql) as $row) {
-  }
-  // Création des constantes
-  define('LOGIN', $row['login']);
-  define('PASSWORD', $row['mdp']);
-
-  // Test champs vide du formulaire : login
-  if (!empty($_POST['nom_du_compte']))
+// Test champs vide du formulaire : login sinon instructions n°1
+if (!empty($_POST['nom_du_compte']))
+{
+  // Test champs c=plein du formulaire : login pour début des vérification avec la base de donnée
+  if (isset($_POST['nom_du_compte']))
   {
-    // est champs vide du formulaire : mdp
-    if(!empty($_POST['mot_de_passe']))
-    {
-      // Sont-ils les mêmes que les constantes ?
-      if($_POST['nom_du_compte'] !== LOGIN)
+    // Requête PHP pour vérifier les informations
+    $sql = "SELECT * FROM users WHERE login = '{$_POST['nom_du_compte']}'";
+    foreach ($bdd->query($sql) as $row) {
+    }
+      // Vérification si le login existe et est lié à un compte sinon instructions n°2
+      if (isset($row['login']))
       {
-        $password_KO = "<div class='alert alert-danger mt-5' role='alert'>Compte inexistant !</div>";
+        // Création des constantes car le compte existe
+        define('LOGIN', $row['login']);
+        define('PASSWORD', $row['mdp']);
+        define('NOM', $row['nom']);
+        define('PRENOM', $row['prenom']);
+
+        // Test champs vide du formulaire : mdp sinon instructions n°3
+        if(!empty($_POST['mot_de_passe']))
+        {
+          //Test si le mdp est incorrect si oui alors instructions n°4
+          if($_POST['mot_de_passe'] !== PASSWORD)
+          {
+            // Instructions n°4
+            $password_KO = "<div class='alert alert-danger mt-5 text-center' role='alert'>Le mot de passe saisi est incorrect.</div>";
+          }
+            // sinon, on ouvre la session
+            else
+          {
+          // On ouvre la session
+          session_start();
+
+          // On enregistre le login en session
+          $_SESSION['login'] = LOGIN;
+          $_SESSION['nom'] = NOM;
+          $_SESSION['prenom'] = PRENOM;
+
+          // On redirige vers le fichier admin.php
+          header('Location: index.php');
+          exit();
+        }
       }
-      else if($_POST['mot_de_passe'] !== PASSWORD)
-      {
-        $password_KO = "<div class='alert alert-danger mt-5' role='alert'>Mot de passe incorrect !</div>";
-      }
+        // Instructions n°3
         else
       {
-        // On ouvre la session
-        session_start();
-
-        // On enregistre le login en session
-        $_SESSION['login'] = LOGIN;
-
-        // On redirige vers le fichier admin.php
-        header('Location: adminSQL.php');
-        exit();
+        $password_KO = "<div class='alert alert-danger mt-5 text-center' role='alert'>Vous devez saisir votre mot de passe pour accéder à votre espace privé.</div>";;
       }
     }
-      else
-    {
-      echo "<div class='alert alert-info mt-5' role='alert'>Indiquez le nom du compte et le mot de passe pour accéder à votre espace privé</div>";;
+
+    // Instructions n°2
+    else {
+      $password_KO = "<div class='alert alert-danger mt-5 text-center' role='alert'>L'identifiant du compte saisi est inexistant.</div>";
     }
   }
+}
+// Message d'instructions n°1 qui s'affiche par défaut
+else
+{
+  $password_KO = "<div class='alert alert-info mt-5 text-center' role='alert'>Indiquez le nom du compte et le mot de passe pour accéder à votre espace privé ou cliquez sur \"créer mon compte privé\" pour créer un nouveau compte.</div>";;
+}
 ?>
 
+<!-- Le HTML -->
+<section class="container bg-light pt-5 pb-5">
+  <h1 class="text-center">Connection à mon espace privé</h1>
+  <hr>
+  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+    <label for="nom_du_compte" class="col-sm-12 col-lg-3 align-top mt-5">Identifiant du compte (Login) :</label>
+    <input type="text" name="nom_du_compte" id="nom_du_compte" class="col-sm-12 col-lg-8 align-top border border-info mt-5">
 
-  <section class="container bg-light pt-5 pb-5">
-    <h1 class="text-center">Connection à mon espace privé</h1>
+    <label for="mot_de_passe" class="col-sm-12 col-lg-3 align-top mt-2">Mot de passe :</label>
+    <input type="password" name="mot_de_passe" id="mot_de_passe" class="col-sm-12 col-lg-8 align-top border border-info mt-2">
 
-    <hr>
+    <input type="submit" name="submit" value="Se connecter à mon compte" class="col-sm-12 offset-lg-4 col-lg-4 mt-3">
+  </form>
 
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-      <label for="nom_du_compte" class="col-sm-12 col-lg-3 align-top">Nom du compte (Login) :</label>
-      <input type="text" name="nom_du_compte" id="nom_du_compte" class="col-sm-12 col-lg-8 align-top border border-info">
+  <div class="col-sm-12 text-center mt-4">
+    <p>ou</p>
+    <a href="creation.php">créer mon compte privé</a>
+  </div>
 
-      <label for="mot_de_passe" class="col-sm-12 col-lg-3 align-top">Mot de passe :</label>
-      <input type="password" name="mot_de_passe" id="mot_de_passe" class="col-sm-12 col-lg-8 align-top border border-info">
+  <?php
+    // Rencontre-t-on une erreur ? Affichage des différentes instructions selon le cas
+    if(!empty($password_KO))
+    {
+      echo $password_KO;
+    }
+  ?>
 
-      <input type="submit" name="submit" value="Se connecter à mon compte" class="col-sm-12 offset-lg-4 col-lg-4">
-    </form>
-
-    <?php
-      // Rencontre-t-on une erreur ?
-      if(!empty($password_KO))
-      {
-        echo $password_KO;
-      }
-    ?>
-
-    <hr>
-
+  <hr>
+  <div class="col-sm-12 text-center mt-4">
     <a href="index.php" class="align-item-center">Revenir au site</a>
-    <hr>
-
+  </div>
+  <hr>
 
 </section>
 
