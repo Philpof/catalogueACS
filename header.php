9770450bca832d2aa079c0bbf13dc2236a760b7f
RDemarch <robin.demarch@sfr.fr>
2020-08-17 16:01:57 +0200

    Connexion à la bdd depuis le header

--- header.php
+++ header.php
@@ -20,3 +20,21 @@
 </head>
 
 <body>
+  <?php
+    try
+    {
+      // On se connecte à MySQL
+      $bdd = new PDO('mysql:host=localhost;dbname=catalogueacs;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    catch(Exception $e)
+    {
+      // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+    }
+
+    function debug($var, $style = "")
+    {
+      echo "<pre style='background-color: white; border: gray 1px solid; -webkit-border-radius: 5px;-moz-border-radius: 5px;border-radius: 5px; color: black; width: 95%; padding: 10px; overflow-y: auto;{$style}'>";
+      var_dump($var);
+      echo "</pre>";
+    }
