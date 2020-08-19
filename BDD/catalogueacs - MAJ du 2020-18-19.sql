abe862367a2826970b8681023ba75cdd07bdc003
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-08-19 22:31:02 +0200

    MAJ de la BDD (modif sur "users")

--- BDD/catalogueacs - MAJ du 2020-18-19.sql
+++ BDD/catalogueacs - MAJ du 2020-18-19.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 17 août 2020 à 11:59
+-- Généré le :  mer. 19 août 2020 à 20:29
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -75,27 +75,30 @@ CREATE TABLE IF NOT EXISTS `users` (
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `dateNaissance` varchar(10) NOT NULL DEFAULT '01/01/2020',
-  `numTel` varchar(255) NOT NULL,
+  `numTel` varchar(10) NOT NULL,
   `email` varchar(255) NOT NULL,
   `adresse` varchar(255) NOT NULL,
-  `cp` int(5) NOT NULL,
+  `cp` varchar(5) NOT NULL,
   `ville` varchar(255) NOT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT 0,
-  `login` varchar(255) NOT NULL,
+  `login` varchar(12) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `nom`, `prenom`, `dateNaissance`, `numTel`, `email`, `adresse`, `cp`, `ville`, `admin`, `login`, `mdp`) VALUES
-(1, 'PERECHODOV', 'Philippe', '24/03/1979', '06 06 06 06 06', 'p.perechodov@codeur.online', 'chez moi', 3460, 'TREVOL', 1, 'phil', 'phil'),
-(2, 'DE MARCH', 'Robin', '08/08/2000', '07 07 07 07 07', 'r.demarch@codeur.online', 'chez toi', 89100, 'MALAY-LE-GRAND', 1, 'robin', 'robin'),
-(3, 'SYED', 'Ali', '21/03/1996', '06 07 06 07 06', 'a.syed@codeur.online', 'chez toi aussi', 58000, 'NEVERS', 1, 'ali', 'ali'),
-(4, 'MINEUR', 'Kevin', '01/01/2005', '07 06 07 06 07', 'kevin.lenoob@gmail.com', 'chez lui', 75005, 'PARIS', 0, 'kev', 'kev'),
-(5, 'MAJEUR', 'Robert', '01/09/1954', '04 86 21 95 56', 'majeur.levieux@free.fr', 'chez lui aussi', 13001, 'MARSEILLE', 0, 'bob', 'bob');
+(1, 'PERECHODOV', 'Philippe', '1979-03-24', '0606060606', 'p.perechodov@codeur.online', '665 impasse de l\'enfer', '03460', 'TREVOL', 1, 'phil', 'phil'),
+(2, 'DE MARCH', 'Robin', '2000-08-08', '0707070707', 'r.demarch@codeur.online', '3 avenue du PHP', '89100', 'MALAY-LE-GRAND', 1, 'robin', 'robin'),
+(3, 'SYED', 'Ali', '1996-03-21', '0607060706', 'a.syed@codeur.online', '32 Bis rue du dev', '58000', 'NEVERS', 1, 'ali', 'ali'),
+(4, 'MINEUR', 'Kevin', '2005-10-15', '0706070607', 'kevin.lenoob@gmail.com', '12 allée du Noob', '75005', 'PARIS', 0, 'kev', 'kev'),
+(5, 'MAJEUR', 'Robert', '1954-09-01', '0486219556', 'majeur.levieux@free.fr', '7892, chemin de la mer, 3eme etg, appt C', '13001', 'MARSEILLE', 0, 'bob', 'bob'),
+(6, 'BON', 'Jean', '0001-01-01', '0123456789', 'jeanbon@boucherie.fr', '01 rue du Hachoir', '71120', 'CHAROLLES', 0, 'Jambon', 'Azerty12'),
+(7, 'TEST', 'Test', '0454-05-04', '0123456789', 'g@g.g', '01 rue du Hachoir', '01234', 'FF', 0, 'Gnouf', 'Azerty12'),
+(8, 'FFFF', 'Sggs', '1651-03-12', '0123456789', 'g@g.j', '01 rue du Hachoir', '01234', 'GID', 0, 'bobb', 'Azerty12');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
