92cb55f0112c19e9b172556af616b22891822ee7
Ali Raza Syed <a.syed@coceur.online>
2020-08-21 14:08:42 +0200

    index.php "nouveux changement fait"

--- index.php
+++ index.php
@@ -4,22 +4,343 @@ include "header.php";
 
 <!-- Le HTML -->
 
-<main>
+<main class="container">
+
+  <div class="slider">
+    <div id="slider" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/days.jpeg" class="d-block w-100">
+      </div>
+      <div class="carousel-item">
+        <img src="img/super.jpg" class="d-block w-100">
+      </div>
+      <div class="carousel-item">
+        <img src="img/zelda1.jpg" class="d-block w-100">
+      </div>
+    <ol class="carousel-indicators">
+      <li data-target="#slider" data-slide-to="0" class="active"></li>
+      <li data-target="#slider" data-slide-to="1"></li>
+      <li data-target="#slider" data-slide-to="2"></li>
+    </ol>
+
+
+    </div>
+    </div>
+  </div>
+
+
+
+    <!-- <div class="row py-3">
+
+
+          <div class="col-3 border-2 bcolor">
+              <div class="w-100 text-center p-4">
+                  <h5 class="p-2">Sponsor</h5>
+                  <img src="img/images.jpeg" alt="zelda" class="img-fluid mt-3">
+                  <img src="img/days.jpeg" alt="zelda" class="img-fluid mt-3">
+
+              </div>
+
+          </div>
+
+    </div> -->
+
 
   <article>
   </article>
 
-  <section>
+  <section class="featured-categories">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-4">
+                  <img src="img/nature.jpg">
+              </div>
+              <div class="col-md-4">
+                  <img src="img/val-y.jpg">
+              </div>
+              <div class="col-md-4">
+                  <img src="img/days.jpeg">
+              </div>
+
+          </div>
+
+      </div>
+  </section>
+  <section class="on-sale">
+  <div class="container">
+    <div class="title-box">
+      <h2>On sale</h2>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="product-top">
+          <img src="img/days.jpeg">
+            <div class="overlay-right">
+              <button type="button" class="btn btn-secondary" title="game shop">
+                <i class="fa fa-eye"></i>
+              </button>
+              <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+                <i class="fa fa-heart-o"></i>
+              </button>
+              <button type="button" class="btn btn-secondary" title="game shop">
+                <i class="fa fa-shopping-cart"></i>
+              </button>
+            </div>
+            </div>
+            <div class="product-bottom text-center">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-half-o"></i>
+                <h3>Days Gone</h3>
+                <h5>$50.00</h5>
+              </div>
+            </div>
+
+  <div class="col-md-3">
+    <div class="product-top">
+      <img src="img/zelda1.jpg">
+        <div class="overlay-right">
+          <button type="button" class="btn btn-secondary" title="game shop">
+            <i class="fa fa-eye"></i>
+          </button>
+          <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+            <i class="fa fa-heart-o"></i>
+          </button>
+          <button type="button" class="btn btn-secondary" title="game shop">
+            <i class="fa fa-shopping-cart"></i>
+          </button>
+        </div>
+        </div>
+        <div class="product-bottom text-center">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+            <h3>Zelda</h3>
+            <h5>$100.00</h5>
+          </div>
+        </div>
+
+<div class="col-md-3">
+  <div class="product-top">
+    <img src="img/assissin.jpg">
+      <div class="overlay-right">
+        <button type="button" class="btn btn-secondary" title="game shop">
+          <i class="fa fa-eye"></i>
+        </button>
+        <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+          <i class="fa fa-heart-o"></i>
+        </button>
+        <button type="button" class="btn btn-secondary" title="game shop">
+          <i class="fa fa-shopping-cart"></i>
+        </button>
+      </div>
+      </div>
+      <div class="product-bottom text-center">
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+          <h3>Assissin</h3>
+          <h5>$1000</h5>
+        </div>
+      </div>
+
+
+      <div class="col-md-3">
+        <div class="product-top">
+          <img src="img/link.jpg">
+            <div class="overlay-right">
+              <button type="button" class="btn btn-secondary" title="game shop">
+                <i class="fa fa-eye"></i>
+              </button>
+              <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+                <i class="fa fa-heart-o"></i>
+              </button>
+              <button type="button" class="btn btn-secondary" title="game shop">
+                <i class="fa fa-shopping-cart"></i>
+              </button>
+            </div>
+            </div>
+            <div class="product-bottom text-center">
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-half-o"></i>
+              <i class="fa fa-star-o"></i>
+                <h3>Link to the past</h3>
+                <h5>$100</h5>
+              </div>
+            </div>
+
+
+        </div>
+
+      </div>
+
   </section>
 
-  <aside>
-  </aside>
+<!-- deuxiéme row -->
+
+<div class="container">
+  <div class="title-box">
+    <h2>Action</h2>
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="product-top">
+        <img src="img/days.jpeg">
+          <div class="overlay-right">
+            <button type="button" class="btn btn-secondary" title="game shop">
+              <i class="fa fa-eye"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+              <i class="fa fa-heart-o"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="game shop">
+              <i class="fa fa-shopping-cart"></i>
+            </button>
+          </div>
+          </div>
+          <div class="product-bottom text-center">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-o"></i>
+              <h3>Days Gone</h3>
+              <h5>$50.00</h5>
+            </div>
+          </div>
+
+<div class="col-md-3">
+  <div class="product-top">
+    <img src="img/zelda1.jpg">
+      <div class="overlay-right">
+        <button type="button" class="btn btn-secondary" title="game shop">
+          <i class="fa fa-eye"></i>
+        </button>
+        <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+          <i class="fa fa-heart-o"></i>
+        </button>
+        <button type="button" class="btn btn-secondary" title="game shop">
+          <i class="fa fa-shopping-cart"></i>
+        </button>
+      </div>
+      </div>
+      <div class="product-bottom text-center">
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+        <i class="fa fa-star"></i>
+          <h3>Zelda</h3>
+          <h5>$100.00</h5>
+        </div>
+      </div>
+
+<div class="col-md-3">
+<div class="product-top">
+  <img src="img/assissin.jpg">
+    <div class="overlay-right">
+      <button type="button" class="btn btn-secondary" title="game shop">
+        <i class="fa fa-eye"></i>
+      </button>
+      <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+        <i class="fa fa-heart-o"></i>
+      </button>
+      <button type="button" class="btn btn-secondary" title="game shop">
+        <i class="fa fa-shopping-cart"></i>
+      </button>
+    </div>
+    </div>
+    <div class="product-bottom text-center">
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+        <h3>Assissin</h3>
+        <h5>$1000</h5>
+      </div>
+    </div>
+
+
+    <div class="col-md-3">
+      <div class="product-top">
+        <img src="img/link.jpg">
+          <div class="overlay-right">
+            <button type="button" class="btn btn-secondary" title="game shop">
+              <i class="fa fa-eye"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="Ad to wishlist">
+              <i class="fa fa-heart-o"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="game shop">
+              <i class="fa fa-shopping-cart"></i>
+            </button>
+          </div>
+          </div>
+          <div class="product-bottom text-center">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-o"></i>
+            <i class="fa fa-star-o"></i>
+              <h3>Link to the past</h3>
+              <h5>$100</h5>
+            </div>
+          </div>
+
+
+      </div>
+
+    </div>
+
+
+  <!-- <aside class="col-3 border-2 bcolor">
+      <div class="w-100 text-center p-4">
+          <h5 class="p-2 border-bt text-danger"></h5>
+
+      </div>
+  </aside> -->
 
 
 </main>
 
 <footer>
 </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php include "footer.php" ?>
