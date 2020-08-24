038946123decd013535329a7dcbc334a36f08e5f
Philpof <philippe.perechodov@free.fr>
2020-08-24 14:34:24 +0200

    modif 404.php

--- 404.php
+++ 404.php
@@ -2,15 +2,13 @@
   include "header.php";
 ?>
 
-<section id="page404" class="container-fluid text-center">
+<section id="page404" class=" vh-100 container-fluid text-center">
 
-<div class="p-5 m-5">
-  <h1>Erreur 404 !</h1>
-  <p> Oops, cette page inexiste pas </p>
-  <p>Cliquez sur Amateratsu et elle vous racompagnera à l'accueil</p>
-    <a href="index.php"><img src="img\page404\error404.jpg" alt="page404"></a>
-</div>
-
-
+  <div class="">
+    <h1>Erreur 404 !</h1>
+    <p> Oops, cette page inexiste pas </p>
+    <p>Cliquez sur Amateratsu et elle vous racompagnera à l'accueil</p>
+      <a href="index.php"><img src="img\page404\error404.jpg" alt="page404"></a>
+  </div>
 
 </section>
