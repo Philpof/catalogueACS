9a8d8209cbc9e924740141a9d14fbade47059c26
Philpof <philippe.perechodov@free.fr>
2020-08-24 14:29:19 +0200

    correction .thaccess

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
 
 <div class="p-5 m-5">
   <h1>Erreur 404 !</h1>
-  <p> Opps, cette page inexiste pas </p>
+  <p> Oops, cette page inexiste pas </p>
   <p>Cliquez sur Amateratsu et elle vous racompagnera à l'accueil</p>
     <a href="index.php"><img src="img\page404\error404.jpg" alt="page404"></a>
 </div>
