be5e7c8998342c4899563be68a5609b970d57885
RDemarch <robin.demarch@sfr.fr>
2020-08-24 11:28:42 +0200

    le carousel est généré par du php

--- index.php
+++ index.php
@@ -5,77 +5,64 @@ include "header.php";
 <!-- Le HTML -->
 
 <main class="container">
-
   <div class="slider">
     <div id="slider" class="carousel slide carousel-fade" data-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="img/days.jpeg" class="d-block w-100">
+<?php
+$nbrIdBG = $bdd->query("SELECT COUNT(id) AS nbrId FROM background");
+$resultat = $nbrIdBG->fetch();
+$nbMax = $resultat['nbrId'];
+
+// Le random avec, en nombre max, le nombre d'entrée déterminée
+$numeroPre = 0;
+for ($cnt = 0; $cnt < 3; $cnt++) {
+  $numero = rand(1, $nbMax);
+  $testPoss = $bdd->query("SELECT id_jeu FROM background WHERE id='{$numero}'");
+  $idTest = $testPoss->fetch();
+  while ($idTest == $numeroPre) {
+    $numero = rand(1, $nbMax);
+  }
+  $imageCarousel = $bdd->query("SELECT * FROM background WHERE id='{$numero}'");
+  while($imageFinal = $imageCarousel->fetch()){
+    $lienImage = $imageFinal['lien'];
+    $idJeu = $imageFinal['id_jeu'];
+  }
+?>
+      <div class="carousel-item <?php if($cnt < 1 ) echo "active";?>">
+        <a href="/catalogueACS/produit.php?idSelect=<?= $idJeu ?>"><img src="<?= $lienImage ?>" class="d-block w-100"></a>
       </div>
-      <div class="carousel-item">
-        <img src="img/super.jpg" class="d-block w-100">
-      </div>
-      <div class="carousel-item">
-        <img src="img/zelda1.jpg" class="d-block w-100">
-      </div>
+<?php
+ $numeroPre = $idJeu;
+}
+
+ ?>
     <ol class="carousel-indicators">
       <li data-target="#slider" data-slide-to="0" class="active"></li>
       <li data-target="#slider" data-slide-to="1"></li>
       <li data-target="#slider" data-slide-to="2"></li>
     </ol>
 
-
-    </div>
-    </div>
   </div>
+  </div>
+</div>
+<?php
 
-
-
-    <!-- <div class="row py-3">
-
-
-          <div class="col-3 border-2 bcolor">
-              <div class="w-100 text-center p-4">
-                  <h5 class="p-2">Sponsor</h5>
-                  <img src="img/images.jpeg" alt="zelda" class="img-fluid mt-3">
-                  <img src="img/days.jpeg" alt="zelda" class="img-fluid mt-3">
-
-              </div>
-
-          </div>
-
-    </div> -->
-
-
-  <article>
-  </article>
-
-  <section class="featured-categories">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-4">
-                  <img src="img/final.jpg">
-              </div>
-              <div class="col-md-4">
-                  <img src="img/mash1.png">
-              </div>
-              <div class="col-md-4">
-                  <img src="img/days.jpeg">
-              </div>
-
-          </div>
-
-      </div>
-  </section>
+?>
   <section class="on-sale">
+
   <div class="container">
     <div class="title-box">
-      <h2>Top</h2>
+      <h2>Best Seller</h2>
     </div>
+<?php
+
+  
+
+?>
     <div class="row">
       <div class="col-md-3">
         <div class="product-top">
-          <a href="#"><img src="img/days.jpeg"></a>
+          <a href="/catalogueACS/produit.php?idSelect=<?= $donnees['id'] ?>"><img src="img/days.jpeg"></a>
             <div class="overlay-right">
               <button type="button" class="btn btn-secondary" title="game shop">
                 <i class="fa fa-eye"></i>
@@ -98,94 +85,13 @@ include "header.php";
                 <h5>$50.00</h5>
               </div>
             </div>
-
-  <div class="col-md-3">
-    <div class="product-top">
-      <a href="#"><img src="img/zelda1.jpg"></a>
-        <div class="overlay-right">
-          <button type="button" class="btn btn-secondary" title="game shop">
-            <i class="fa fa-eye"></i>
-          </button>
-          <button type="button" class="btn btn-secondary" title="Ad to wishlist">
-            <i class="fa fa-heart-o"></i>
-          </button>
-          <button type="button" class="btn btn-secondary" title="game shop">
-            <i class="fa fa-shopping-cart"></i>
-          </button>
-        </div>
-        </div>
-        <div class="product-bottom text-center">
-          <i class="fa fa-star"></i>
-          <i class="fa fa-star"></i>
-          <i class="fa fa-star"></i>
-          <i class="fa fa-star"></i>
-          <i class="fa fa-star"></i>
-            <h3>Zelda</h3>
-            <h5>$100.00</h5>
-          </div>
-        </div>
-
-<div class="col-md-3">
-  <div class="product-top">
-    <a href="#"><img src="img/assissin.jpg"></a>
-      <div class="overlay-right">
-        <button type="button" class="btn btn-secondary" title="game shop">
-          <i class="fa fa-eye"></i>
-        </button>
-        <button type="button" class="btn btn-secondary" title="Ad to wishlist">
-          <i class="fa fa-heart-o"></i>
-        </button>
-        <button type="button" class="btn btn-secondary" title="game shop">
-          <i class="fa fa-shopping-cart"></i>
-        </button>
-      </div>
-      </div>
-      <div class="product-bottom text-center">
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-          <h3>Assissin</h3>
-          <h5>$1000</h5>
-        </div>
-      </div>
-
-
-      <div class="col-md-3">
-        <div class="product-top">
-          <a href="#"><img src="img/link.jpg"></a>
-            <div class="overlay-right">
-              <button type="button" class="btn btn-secondary" title="game shop">
-                <i class="fa fa-eye"></i>
-              </button>
-              <button type="button" class="btn btn-secondary" title="Ad to wishlist">
-                <i class="fa fa-heart-o"></i>
-              </button>
-              <button type="button" class="btn btn-secondary" title="game shop">
-                <i class="fa fa-shopping-cart"></i>
-              </button>
-            </div>
-            </div>
-            <div class="product-bottom text-center">
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star-half-o"></i>
-              <i class="fa fa-star-o"></i>
-                <h3>Link to the past</h3>
-                <h5>$100</h5>
-              </div>
-            </div>
-
-
         </div>
 
       </div>
 
   </section>
 
-<!-- deuxiéme row -->
+<!-- deuxième row -->
 <section class="new-product">
 
 </section>
@@ -354,7 +260,7 @@ include "header.php";
 
       <div class="col-md-3">
       <div class="product-top">
-        <a href="#"><img src="img/assissin.jpg"></a> 
+        <a href="#"><img src="img/assissin.jpg"></a>
           <div class="overlay-right">
             <button type="button" class="btn btn-secondary" title="game shop">
               <i class="fa fa-eye"></i>
